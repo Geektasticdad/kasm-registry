c2c9b817eca91e1a55c6077171fa41c0ba78ccc1
Geektasticdad <73676044+Geektasticdad@users.noreply.github.com>
2023-05-08 19:14:07 -0700

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -2,11 +2,11 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
+    name: 'Geektasticdad Kasm Registry',
+    description: 'The Geektasticdad Kasm Registry',
     icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
-    contactUrl: 'https://kasmweb.com/support',
+    listUrl: 'https://username.github.io/kasm-registry',
+    contactUrl: 'https://github.com/geektasticdad/kasm-registry/issues',
   },
   reactStrictMode: true,
   swcMinify: true,
